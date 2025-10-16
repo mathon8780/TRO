7cf40b5cb3e20520e6edaecf69dbcfd4dc5ed98b
Mathon <mathon.8780@outlook.com>
2025-10-16 16:40:06 +0800

    Init Commit

---
+++ Assets/Scripts/CoreComponents/MovementComponent.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace CoreComponents
+{
+    /// <summary>
+    /// 玩家移动组件
+    /// </summary>
+    public class MovementComponent : MonoBehaviour
+    {
+        
+    }
+}
